f450f43f475eebb97010ad6e4f10f17530fe5123
Floppa <jaxonwindemuller101@gmail.com>
2026-09-21 10:00:07 -0400

    Improve Steller brain unknown handling and memory

--- core/brain.js
+++ core/brain.js
@@ -9,29 +9,43 @@ export class StellerBrain {
     understand(text) {
         const data = analyze(text);
         const concepts = [];
+        const unknown = [];
 
         for (const token of data.tokens) {
             if (this.knowledge[token]) {
                 concepts.push(this.knowledge[token]);
+            } else {
+                unknown.push(token);
             }
         }
 
         return {
             ...data,
-            concepts
+            concepts,
+            unknown
         };
+    }
+
+    learn(word, data) {
+        this.knowledge[word.toLowerCase()] = data;
     }
 
     think(text) {
         const thought = this.understand(text);
 
         if (!thought.concepts.length) {
-            return "I do not know that yet, but I can learn it.";
+            return `I don't know about "${thought.tokens.join(' ')}" yet. My knowledge can be expanded by teaching me new concepts.`;
         }
 
-        return thought.concepts
+        const answer = thought.concepts
             .map(c => c.explain)
             .filter(Boolean)
             .join(' ');
+
+        if (thought.unknown.length) {
+            return `${answer}\n\nI also noticed these unknown tokens: ${thought.unknown.join(', ')}.`;
+        }
+
+        return answer;
     }
 }
